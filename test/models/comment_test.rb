9f8f730e259b6ecdf9eaede14e516d25ef1cbe28
sylvain <syl-p@ik.me>
2025-06-03 11:02:27 +0200

    ci: prepare some test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -6,6 +6,12 @@ class CommentTest < ActiveSupport::TestCase
     assert comment.valid?
   end
 
+  test "comment should have content" do
+    comment = FactoryBot.build(:comment, content: nil)
+    assert_not comment.valid?
+    assert_includes comment.errors[:content], "can't be blank"
+  end
+
   test "should associate comment to a commentable model" do
     comment = FactoryBot.create(:comment)
     activity = FactoryBot.create(:activity)
@@ -16,13 +22,18 @@ class CommentTest < ActiveSupport::TestCase
   end
 
   test "reply should be valid" do
-    parent_comment = FactoryBot.create(:comment)
-    reply = FactoryBot.build(:comment, parent_id: parent_comment.id)
-    reply.save
+    commentable = FactoryBot.create(:activity)
+    parent_comment = FactoryBot.build(:comment)
+    parent_comment.commentable = commentable
+    parent_comment.save!
 
-    assert reply.valid?
+
+    reply = Comment.new(content: 'This is a reply', commentable:, parent_id: parent_comment.id)
+    
+    parent_comment.replies << reply
+    
     assert_includes parent_comment.replies, reply
     assert_equal reply.parent_id, parent_comment.id
-    assert_equal reply.commentable, parent_comment.commentable
+    assert_equal reply.commentable, commentable
   end
 end
