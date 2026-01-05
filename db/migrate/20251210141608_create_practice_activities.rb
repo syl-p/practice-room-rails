40766ac370f47e5c45b2c72b3eba92d196c8ab41
Sylvain <sylvain.pastor@top-webgroup.com>
2026-01-05 09:13:35 +0100

    lint: rubocop -a

--- db/migrate/20251210141608_create_practice_activities.rb
+++ db/migrate/20251210141608_create_practice_activities.rb
@@ -7,6 +7,6 @@ class CreatePracticeActivities < ActiveRecord::Migration[8.0]
       t.timestamps
     end
 
-    add_index :practice_activities, [:practice_id, :activity_id], unique: true
+    add_index :practice_activities, [ :practice_id, :activity_id ], unique: true
   end
 end
--- test/factories/practice_activities.rb
+++ test/factories/practice_activities.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :practice_activity do
-    
   end
 end
