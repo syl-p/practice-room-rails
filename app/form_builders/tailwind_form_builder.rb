5956c14c60f2830fc5653447181f8a1ba32ae273
Sylvain <sylvain.pastor@top-webgroup.com>
2026-01-22 13:57:22 +0100

    build custom design system

--- app/form_builders/tailwind_form_builder.rb
+++ app/form_builders/tailwind_form_builder.rb
@@ -1,32 +1,30 @@
 class TailwindFormBuilder < ActionView::Helpers::FormBuilder
   def text_field(method, options = {})
     form_group(method, options) do
-      super(method, merge_default_classes(options, "form-input"))
+      super(method, merge_default_classes(options, "form__input"))
     end
   end
 
   def email_field(method, options = {})
     form_group(method, options) do
-      super(method, merge_default_classes(options, "form-input"))
+      super(method, merge_default_classes(options, "form__input"))
     end
   end
 
-  (method, options = {})
-
   def text_area(method, options = {})
     form_group(method, options) do
-      super(method, merge_default_classes(options, "form-input"))
+      super(method, merge_default_classes(options, "form__input"))
     end
   end
 
   def password_field(method, options = {})
     form_group(method, options) do
-      super(method, merge_default_classes(options, "form-input"))
+      super(method, merge_default_classes(options, "form__input"))
     end
   end
 
   def submit(value = nil, options = {})
-    options[:class] ||= "btn btn-primary"
+    options[:class] ||= "btn btn--primary"
     @template.content_tag(:div) do
       super(value, options)
     end
@@ -37,10 +35,10 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
   def form_group(method, options, &block)
     label_text = options.delete(:label) { method.to_s.humanize } if options.present?
     error_messages = @object.errors[method].map { |msg| "#{label_text} #{msg}" }.join(", ") if @object && @object.errors
-    error_span = error_messages.present? ? @template.content_tag(:span, error_messages, class: "text-alert") : "".html_safe
+    error_span = error_messages.present? ? @template.content_tag(:span, error_messages, class: "text-error") : "".html_safe
 
-    @template.content_tag(:div, class: "form-group mb-3 #{'has-error' if error_messages.present?}") do
-      @template.concat @template.label(@object_name, method, label_text, class: "form-label")
+    @template.content_tag(:div, class: "form__group mb-3 #{'has-error' if error_messages.present?}") do
+      @template.concat @template.label(@object_name, method, label_text, class: "form__label")
       @template.concat(block.call)
       @template.concat(error_span)
     end
--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -5,10 +5,10 @@ module TailwindHelper
   LABEL_CLASSES = "block mb-2 text-sm font-medium text-gray-900"
   ERROR_CLASSES = "text-sm text-red-500 mb-3"
 
-  BTN_BASE_CLASSES = " inline-flex items-center gap-3 justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 "
-  PRIMARY_CLASSES = " bg-primary text-primary-foreground hover:bg-primary/80 "
-  SECONDARY_CLASSES = " bg-secondary text-secondary-foreground hover:bg-secondary/80 "
-  OUTLINE_CLASSES = "  border border-input bg-background hover:bg-accent hover:text-accent-foreground "
-  GHOST_CLASSES = " hover:bg-accent hover:text-accent-foreground  "
-  DESTRUCTIVE_CLASSES = " bg-destructive text-destructive-foreground hover:bg-destructive/90 "
+  BTN_BASE_CLASSES = " btn "
+  PRIMARY_CLASSES = " btn--primary "
+  SECONDARY_CLASSES = " bg-secondary "
+  OUTLINE_CLASSES = " btn--outline "
+  GHOST_CLASSES = " btn--link "
+  DESTRUCTIVE_CLASSES = " btn--error "
 end
